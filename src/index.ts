81a5b60fea481e88ee37054eb9f8c640fa3dd786
wangxiang <wangxiang@tangiplay.com>
2021-06-16 12:04:16 +0800

    express@4.17.1 not include Locals param

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import { NextFunction, RequestHandler } from 'express';
 import { ParamsDictionary, Query } from 'express-serve-static-core';
 
-export default function asyncHandler<
-  P = ParamsDictionary,
-  ResBody = any,
-  ReqBody = any,
-  ReqQuery = Query,
-  Locals extends Record<string, any> = Record<string, any>,
->(fn: RequestHandler<P, ResBody, ReqBody, ReqQuery, Locals>): RequestHandler<P, ResBody, ReqBody, ReqQuery, Locals> {
+export default function asyncHandler<P = ParamsDictionary, ResBody = any, ReqBody = any, ReqQuery = Query>(
+  fn: RequestHandler<P, ResBody, ReqBody, ReqQuery>,
+): RequestHandler<P, ResBody, ReqBody, ReqQuery> {
   return function asyncHandlerWrap(...args) {
     const fnReturn = fn(...args);
     const next = args[args.length - 1] as NextFunction;
